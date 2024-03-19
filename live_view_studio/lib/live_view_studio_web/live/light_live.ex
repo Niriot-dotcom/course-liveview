9ac4df0ef058087f3b08c5acc2aab156c0671f56
niriot-dotcom <72941421+Niriot-dotcom@users.noreply.github.com>
2024-03-18 19:32:13 -0600

    light and its temperature color.

--- live_view_studio/lib/live_view_studio_web/live/light_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/light_live.ex
@@ -1,10 +1,16 @@
 defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
+  # utils functions
+  defp temp_color("3000"), do: "#F1C40D"
+  defp temp_color("4000"), do: "#FEFF66"
+  defp temp_color("5000"), do: "#99CCFF"
+
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
-        brightness: 10
+        brightness: 10,
+        temp: "3000"
       )
 
     {:ok, socket}
@@ -15,10 +21,10 @@ defmodule LiveViewStudioWeb.LightLive do
     <h1>Focus Light Render</h1>
     <div id="light">
       <div class="meter">
-        <span style={"width: #{@brightness}%;"}><%= assigns.brightness %>%</span>
+        <span style={"width: #{@brightness}%; background-color: #{temp_color(@temp)}"}><%= assigns.brightness %>%</span>
       </div>
 
-    <%!-- actions --%>
+      <%!-- actions --%>
       <button phx-click="off">
         <img src="/images/light-off.svg" />
       </button>
@@ -38,6 +44,23 @@ defmodule LiveViewStudioWeb.LightLive do
       <button phx-click="fire">
         <img src="/images/fire.svg" />
       </button>
+
+      <%!-- slider --%>
+      <form phx-change="slide">
+        <input type="range" min="0" max="100" name="brightness" value={@brightness} />
+      </form>
+
+      <%!-- temperature --%>
+      <form phx-change="update-temp">
+        <div class="temps">
+          <%= for temp <- ["3000", "4000", "5000"] do %>
+            <div>
+              <input type="radio" id={temp} name="temp" value={temp} checked={temp === @temp} />
+              <label for={temp}><%= temp %></label>
+            </div>
+          <% end %>
+        </div>
+      </form>
     </div>
     """
   end
@@ -66,5 +89,17 @@ defmodule LiveViewStudioWeb.LightLive do
   def handle_event("fire", _, socket) do
     socket = assign(socket, brightness: Enum.random(0..100))
     {:noreply, socket}
+  end
+
+  def handle_event("slide", params, socket) do
+    %{"brightness" => b} = params
+    # i didn't need to convert, why? ==> String.to_integer(brightness))
+    socket = assign(socket, brightness: b)
+    {:noreply, socket}
+  end
+
+  def handle_event("update-temp", params, socket) do
+    %{"temp" => t} = params
+    {:noreply, assign(socket, temp: t)}
   end
 end
