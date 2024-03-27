92b32a8e8d0575de1aa9eb8eef464a4cad245a2e
niriot-dotcom <72941421+Niriot-dotcom@users.noreply.github.com>
2024-03-26 19:55:21 -0600

    function components in heex templates.

--- live_view_studio/lib/live_view_studio_web/live/boats_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/boats_live.ex
@@ -42,6 +42,16 @@ defmodule LiveViewStudioWeb.BoatsLive do
           </div>
         </div>
       </form>
+
+      <.promo hours_expiration={2}>
+        Save 25% on rentals
+
+        <:legal>
+          <Heroicons.exclamation_circle />
+          Limit 1 per party
+        </:legal>
+      </.promo>
+
       <div class="boats">
         <div :for={boat <- @boats} class="boat">
           <img src={boat.image} />
@@ -59,6 +69,38 @@ defmodule LiveViewStudioWeb.BoatsLive do
             </div>
           </div>
         </div>
+      </div>
+
+      <.promo>
+        Hurry, only 3 boats left!
+
+        <:legal>
+          Excluding weekends
+        </:legal>
+      </.promo>
+    </div>
+    """
+  end
+
+  # types (requirements)
+  attr(:hours_expiration, :integer, default: 24)
+  slot(:legal)
+  slot(:inner_block, required: true)
+
+  # function components
+  def promo(assigns) do
+    ~H"""
+    <div class="promo">
+      <div class="deal">
+        <%= render_slot(@inner_block) %>
+      </div>
+
+      <div class="expiration">
+        DEAL EXPIRES IN <%= @hours_expiration %> HOURS
+      </div>
+
+      <div class="legal">
+        <%= render_slot(@legal) %>
       </div>
     </div>
     """
