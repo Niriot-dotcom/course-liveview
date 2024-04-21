417a0f9fb8604664e88a0232a62e1bb4f4a6265e
Paty Lopez <lopezpatyyarely@gmail.com>
2024-04-21 12:39:31 -0600

    Servy Handler module until chapter 8.
    - high-level transformations
    - pattern matching (also advanced & maps)
    - immutable data
    - function clauses

---
+++ elixir-course/servy/lib/servy/handler.ex
@@ -0,0 +1,166 @@
+defmodule Servy.Handler do
+  require Logger
+
+  def handle(request) do
+    request
+    |> parse
+    |> rewrite_path
+    |> log
+    |> route
+    |> emojify
+    |> track
+    |> format_response
+  end
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(%{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
+
+  defp rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{conv | path: "/#{thing}/#{id}"}
+  end
+
+  defp rewrite_path_captures(conv, nil), do: conv
+
+  def track(%{status: 404, path: path} = conv) do
+    Logger.warning("⚠️ Warning: #{path} is on the loose.")
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def parse(request) do
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: "", status: nil}
+  end
+
+  def log(conv), do: IO.inspect(conv)
+
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{conv | status: 200, resp_body: "Todd, Snow, Firulais"}
+  end
+
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bear #{id} has been deleted."}
+  end
+
+  def route(conv) do
+    %{conv | status: 404, resp_body: "No #{conv.method} #{conv.path} here!"}
+  end
+
+  def emojify(%{status: 200, resp_body: resp_body} = conv) do
+    %{conv | resp_body: "🎉\n" <> resp_body <> "\n🎉"}
+  end
+
+  def emojify(conv), do: conv
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    Content-Type: text/html
+    Content-Length: #{byte_size(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
+
+request = """
+GET /bears/123 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
+
+request = """
+DELETE /bears/2 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
+
+request = """
+GET /bears?id=3 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
