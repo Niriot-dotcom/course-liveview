2890db7a2c2320ba44358ef8809abac74e78a997
Paty Lopez <lopezpatyyarely@gmail.com>
2024-04-22 21:40:07 -0600

    [elixir-otp] chapter 9 - 12.
    - serving files (about, contact & form HTML files)
    - module attributes (and docs like @moduledoc & @doc)
    - organizing code (plugins & parser)
    - modeling with structs (typed maps with tuple {:key, value})

--- elixir-course/servy/lib/servy/handler.ex
+++ elixir-course/servy/lib/servy/handler.ex
@@ -1,6 +1,16 @@
 defmodule Servy.Handler do
-  require Logger
+  @moduledoc "Handles HTTP requests."
 
+  import Servy.Parser, only: [parse: 1]
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
+
+  alias Servy.Conv
+
+  @pages_path Path.expand("../../pages/", __DIR__)
+  # @pages_path Path.expand("pages", File.cwd!)
+
+  @doc "Transforms the request into a response."
   def handle(request) do
     request
     |> parse
@@ -12,86 +22,74 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(%{path: path} = conv) do
-    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
-    captures = Regex.named_captures(regex, path)
-    rewrite_path_captures(conv, captures)
-  end
-
-  defp rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
-    %{conv | path: "/#{thing}/#{id}"}
-  end
-
-  defp rewrite_path_captures(conv, nil), do: conv
-
-  def track(%{status: 404, path: path} = conv) do
-    Logger.warning("⚠️ Warning: #{path} is on the loose.")
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
-
-    %{method: method, path: path, resp_body: "", status: nil}
-  end
-
-  def log(conv), do: IO.inspect(conv)
-
-  def route(%{method: "GET", path: "/wildthings"} = conv) do
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     %{conv | status: 200, resp_body: "Todd, Snow, Firulais"}
   end
 
-  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
+    file =
+      @pages_path
+      |> Path.join("form.html")
+
+    # |> File.read()
+    # |> handle_file(conv)
+
+    case File.read(file) do
+      {:ok, content} ->
+        %{conv | status: 200, resp_body: content}
+
+      {:error, :enoent} ->
+        %{conv | status: 404, resp_body: "File not found."}
+
+      {:error, reason} ->
+        %{conv | status: 500, resp_body: "File error: #{reason}"}
+    end
+  end
+
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
+  def route(%Conv{method: "DELETE", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id} has been deleted."}
   end
 
-  def route(conv) do
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
+    @pages_path
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
+  def route(%Conv{method: "GET", path: "/pages/" <> html_file} = conv) do
+    @pages_path
+    |> Path.join("#{html_file}.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
+  def route(%Conv{} = conv) do
     %{conv | status: 404, resp_body: "No #{conv.method} #{conv.path} here!"}
   end
 
-  def emojify(%{status: 200, resp_body: resp_body} = conv) do
+  def emojify(%Conv{status: 200, resp_body: resp_body} = conv) do
     %{conv | resp_body: "🎉\n" <> resp_body <> "\n🎉"}
   end
 
-  def emojify(conv), do: conv
+  def emojify(%Conv{} = conv), do: conv
 
   def format_response(conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
-  end
-
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
   end
 end
 
@@ -157,6 +155,36 @@ request |> Servy.Handler.handle() |> IO.puts()
 
 request = """
 GET /bears?id=3 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
+
+request = """
+GET /bears/new HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+request |> Servy.Handler.handle() |> IO.puts()
+
+request = """
+GET /pages/contact HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
