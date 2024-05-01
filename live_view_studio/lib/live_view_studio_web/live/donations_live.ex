a3592d008285830a48e327980b0c535542e11630
Paty Lopez <lopezpatyyarely@gmail.com>
2024-05-01 16:05:34 -0600

    [phx-liveview] sorting donations and pizza.

--- live_view_studio/lib/live_view_studio_web/live/donations_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/donations_live.ex
@@ -4,13 +4,72 @@ defmodule LiveViewStudioWeb.DonationsLive do
   alias LiveViewStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
+    {:ok, socket, temporary_assigns: [donations: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    # sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    # sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
+    sort_by = valid_sort_by(params)
+    sort_order = valid_sort_order(params)
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    donations = Donations.list_donations(options)
 
     socket =
       assign(socket,
-        donations: donations
+        donations: donations,
+        options: options
       )
 
-    {:ok, socket}
+    {:noreply, socket}
   end
+
+  # function component
+  attr(:sort_by, :atom, required: true)
+  attr(:options, :map, required: true)
+  slot(:inner_block, required: true)
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"}
+    >
+      <%= render_slot(@inner_block) %>
+      <%= if @options.sort_by == @sort_by do %>
+        <%= if @options.sort_order == :asc do %>
+          <span>👆</span>
+        <% else %>
+          <span>👇</span>
+        <% end %>
+      <% end %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(actual_order) do
+    case actual_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
+  end
+
+  defp valid_sort_by(%{"sort_by" => sort_by})
+       when sort_by in ~w(item quantity days_until_expires) do
+    String.to_atom(sort_by)
+  end
+
+  defp valid_sort_by(_params), do: :id
+
+  defp valid_sort_order(%{"sort_order" => sort_order})
+       when sort_order in ~w(asc desc) do
+    String.to_atom(sort_order)
+  end
+
+  defp valid_sort_order(_params), do: :asc
 end
--- live_view_studio/lib/live_view_studio_web/live/pizza_orders_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/pizza_orders_live.ex
@@ -5,11 +5,67 @@ defmodule LiveViewStudioWeb.PizzaOrdersLive do
   import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(socket,
-        pizza_orders: PizzaOrders.list_pizza_orders()
-      )
-
     {:ok, socket}
   end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = valid_sort_by(params)
+    sort_order = valid_sort_order(params)
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    socket =
+      assign(socket,
+        pizza_orders: PizzaOrders.list_pizza_orders(options),
+        options: options
+      )
+
+    {:noreply, socket}
+  end
+
+  # function component
+  attr(:sort_by, :atom, required: true)
+  attr(:options, :map, required: true)
+  slot(:inner_block, required: true)
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/pizza-orders?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"}
+    >
+      <%= render_slot(@inner_block) %>
+      <%= if @options.sort_by == @sort_by do %>
+        <%= if @options.sort_order == :asc do %>
+          <span>👆</span>
+        <% else %>
+          <span>👇</span>
+        <% end %>
+      <% end %>
+    </.link>
+    """
+  end
+
+  defp next_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
+  end
+
+  defp valid_sort_by(%{"sort_by" => sort_by})
+       when sort_by in ~w(size style topping_1 topping_2 price) do
+    String.to_atom(sort_by)
+  end
+
+  defp valid_sort_by(_params), do: :id
+
+  defp valid_sort_order(%{"sort_order" => sort_order})
+       when sort_order in ~w(asc desc) do
+    String.to_atom(sort_order)
+  end
+
+  defp valid_sort_order(_params), do: :asc
 end
